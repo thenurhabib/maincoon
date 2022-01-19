b8f23180ba5bc0ac3f2c5a4f03f9940215ee4185
Md. Nur habib <68025241+thenurhabib@users.noreply.github.com>
2022-01-19 21:33:50 +0600

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-class AllTools:
-
-    def whois(wh):
-        url = wh
-        result = requests.get("http://api.hackertarget.com/whois/?q=" + url).text
-        print("\n" + result + "\n")
